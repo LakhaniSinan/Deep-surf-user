d4b83a6d6e0bda9201ce869106a4ca1f4c7b6d08
Hammas2580 <hasirali653@gmail.com>
2026-01-26 12:03:51 +0500

    tading card compont

--- src/app/newAiTools/aiMarketRegion/market.jsx
+++ src/app/newAiTools/aiMarketRegion/market.jsx
@@ -1,12 +1,60 @@
-import { Box } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import React from 'react'
 import TradingCard from '../../../components/tradeCard'
 
 const Market = () => {
+    const tardingCardData = [
+        {
+            name: "BTC Price",
+            value: "$94,536",
+            percentage: "-2.14%",
+            titleColor: "neutral.cadetGrey",
+            valueColor: "text.almostWhite",
+            perColor : "text.carmineRed"
+        },
+        {
+            name: "Funding Rate",
+            value: "+0.0025%",
+            percentage: "Longs Pay",
+            titleColor: "neutral.cadetGrey",
+            valueColor: "neutral.cadetGrey"
+
+        },
+        {
+            name: "RSI (14)",
+            value: "64",
+            percentage: "Neutral",
+            titleColor: "neutral.cadetGrey",
+            valueColor: "text.almostWhite"
+
+
+        },
+        {
+            name: "Open Interest",
+            value: "$0.00B",
+            percentage: "Total OI",
+            titleColor: "neutral.cadetGrey",
+            valueColor: "text.almostWhite"
+
+        },
+    ]
     return (
         <Box>
-            <TradingCard/>
+            <Grid container spacing={1}>
+                {tardingCardData.map((item, index) => (
+                    <Grid key={index} size={{ xs: 12, md: 3 }}>
+                        <Box>
+                            <TradingCard
+                                title={item.name}
+                                value={item.value}
+                                time={item.percentage}
+                            />
+                        </Box>
+                    </Grid>
+                ))}
+            </Grid>
         </Box>
+
     )
 }
 
