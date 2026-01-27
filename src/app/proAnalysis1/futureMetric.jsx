2d30b240368f4f6a45cfc32485d0dbf3ecc5c46d
Hammas2580 <hasirali653@gmail.com>
2026-01-27 14:51:32 +0500

    futureMetric work is done

--- src/app/proAnalysis1/futureMetric.jsx
+++ src/app/proAnalysis1/futureMetric.jsx
@@ -1,8 +1,10 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import RealodIcon from "../../assets/icons/relaod-Icon.svg"
 import HeaderText from '../../components/headerText'
 import CustomInput from '../../components/customInput'
 import SearchIcon from "../../assets/icons/Vector.svg"
+import ProgressBar from '../../components/progressBar'
+import AccordingHeader from '../../components/accordingHeader'
 
 
 const FutureMetric = () => {
@@ -10,18 +12,65 @@ const FutureMetric = () => {
         {
             title: "Funding Rate",
             value: "0.000%",
-            symbol: "Neutral"
+            symbol: "Neutral",
+            valueColor: 'neutral.Snowwhite',
+            symbolcolor: "neutral.brightYellow"
         },
-         {
+        {
             title: "Open Interest",
             value: "1.5M",
-            symbol: "+3.70% (24h)"
+            symbol: "+3.70% (24h)",
+            valueColor: 'neutral.Snowwhite',
+            symbolcolor: "neutral.brightGreen"
+
+
         },
         {
             title: "L/S Positions",
             value: "2.34",
-            symbol: <Prog
+            description: "Everyone is longing ETH. Be careful!",
+            valueColor: 'neutral.Snowwhite',
+            decriptionColor: "neutral.brightYellow",
+            symbol:
+                <ProgressBar value={100}
+                    sx={{
+                        "& .MuiLinearProgress-bar": {
+                            backgroundColor: "neutral.brightGreen",
+                            borderRadius: 2,
+                        },
+                    }} />,
+
         }
+        ,
+        {
+            title: "L/S Accounts",
+            value: "2.75",
+            valueColor: 'neutral.Snowwhite',
+            symbol:
+                <ProgressBar value={100}
+                    sx={{
+                        "& .MuiLinearProgress-bar": {
+                            backgroundColor: "neutral.brightGreen",
+                            borderRadius: 2,
+                        },
+                    }} />,
+
+        },
+        {
+            title: "Cumulative Delta",
+            value: "+0.20M",
+            description: "Buying is predominant",
+            valueColor: 'neutral.brightGreen',
+            decriptionColor: "neutral.brightGreen"
+
+
+        }
+    ]
+    const heatMapData = [
+        " Funding > 0.01% = Longs pay shorts (market overheating) ",
+        "  L/S > 1.5 = Dangerous skew towards LONG (correction possible) ",
+        " OI rising + price rising = strong upward trend ",
+        " OI falling = position closures, reversal possible"
     ]
     return (
         <Box bgcolor={"background.charcoal"} p={2} mt={2} borderRadius={"10px"}>
@@ -51,6 +100,45 @@ const FutureMetric = () => {
                 <Typography textAlign={"center"} color='neutral.Snowwhite' fontSize={"30px"} fontWeight={600}>
                     $4,321.7
                 </Typography>
+            </Box>
+            <Box mt={2}>
+                <Grid container spacing={1}>
+                    {futureMetricData.map((item) => (
+                        <Grid size={{ xs: 12, md: 2.3 }}>
+                            <Box bgcolor={"neutral.darkGrey"} borderRadius={"18px"} p={1.5} height={"170px"}>
+                                <Typography color='neutral.Snowwhite' fontSize={"20px"} fontWeight={400}>
+                                    {item.title}
+                                </Typography>
+                                <Typography color={item.valueColor} mt={1} fontSize={"28px"} fontWeight={600}>
+                                    {item.value}
+                                </Typography>
+                                <Typography color={item.symbolcolor} mt={2}>
+                                    {item.symbol}
+                                </Typography>
+                                <Typography color={item.decriptionColor} fontSize={"11px"}>
+                                    {item.description}
+                                </Typography>
+                            </Box>
+
+                        </Grid>
+                    ))}
+
+                </Grid>
+            </Box>
+            <Box bgcolor={"neutral.darkGrey"} borderRadius={"18px"} p={2.5} mt={2}>
+                <Box>
+                    <Typography color='rgba(141, 141, 141, 1)'>
+                        How to read:
+                    </Typography>
+                    <Box>
+                        {heatMapData.map((item, index) => (
+                            <Box  >
+                                <AccordingHeader mt={0} tittle={item} bgGroundColor={"rgba(141, 141, 141, 1)"} color={"rgba(141, 141, 141, 1)"} key={index} lineHeight={"1px"} />
+                            </Box>
+                        ))}
+                    </Box>
+                </Box>
+
             </Box>
         </Box>
     )
--- src/components/accordingHeader/index.jsx
+++ src/components/accordingHeader/index.jsx
@@ -1,25 +1,25 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
 
-const AccordingHeader = ({ tittle, fontSize }) => {
+const AccordingHeader = ({ tittle, fontSize, color, bgGroundColor, mt = 1 }) => {
     return (
-        <Box mt={2}>
+        <Box mt={mt}>
 
-            <Box display="flex" alignItems="flex-start" gap="10px" mt={0.2} >
-
+            <Box display="flex" alignItems="flex-start" gap="10px" mt={0.2} lineHeight="1px"
+            >
                 <Box
                     sx={{
                         width: 8,
                         height: 8,
                         borderRadius: "50%",
-                        backgroundColor: "#fff",
+                        backgroundColor: bgGroundColor || "#fff",
                         mt: "6px",
                         flexShrink: 0,
                     }}
                 />
 
                 {/* Text */}
-                <Typography fontSize={fontSize || "12px"} color="neutral.Snowwhite">
+                <Typography fontSize={fontSize || "12px"} color={color || "neutral.Snowwhite"}>
                     {tittle}
                 </Typography>
             </Box>
